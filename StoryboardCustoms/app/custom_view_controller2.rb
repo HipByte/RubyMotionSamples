fa96d0546174821faabc1cb190b8ba346a83ec94
René Köcher <shirk@bitspin.org>
2012-05-18 17:28:45 +0200

    Make sure to load self.view when awaking from NIB.

--- StoryboardCustoms/app/custom_view_controller2.rb
+++ StoryboardCustoms/app/custom_view_controller2.rb
@@ -3,24 +3,27 @@ class CustomViewController2 < UIViewController
   TAG_DESCRIPTION = 2
   TAG_CUSTOMTEXT  = 3
 
+  def awakeFromNib
+    # make sure our views are loaded
+    self.view
+  end
+
   def viewDidLoad
     puts "#{self.to_s}: CustomViewController2::viewDidLoad"
     self.title = "CustomVC2"
-
-    @idlabel ||= retrieveSubviewWithTag(self, TAG_IDLABEL)
-    @idlabel.text = self.to_s
     
-    @description ||= retrieveSubviewWithTag(self, TAG_DESCRIPTION)
-    @customtext ||= retrieveSubviewWithTag(self, TAG_CUSTOMTEXT)
+    @idlabel = retrieveSubviewWithTag(self, TAG_IDLABEL)
+    @idlabel.text = self.to_s
+
+    @description = retrieveSubviewWithTag(self, TAG_DESCRIPTION)
+    @customtext = retrieveSubviewWithTag(self, TAG_CUSTOMTEXT)
   end
 
   def setParentId(id)
-    @description ||= retrieveSubviewWithTag(self, TAG_DESCRIPTION)
     @description.text = "Custom text from #{id}:"
   end
 
   def customtext
-    @customtext ||= retrieveSubviewWithTag(self, TAG_CUSTOMTEXT)
     @customtext
   end
 end
