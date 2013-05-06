90a64be737c6bb2ce65162fcb32330b9c5bf527a
Laurent Sansonetti <lrz@hipbyte.com>
2013-05-07 00:29:57 +0200

    fix style

--- osx/RssReader/app/controller.rb
+++ osx/RssReader/app/controller.rb
@@ -1,44 +1,43 @@
 class RssReaderController
-
   def initialize
     @data = []
 
     @text_url = NSTextField.alloc.initWithFrame(NSMakeRect(11, 330, 400, 22))
-    @text_url.setStringValue("http://images.apple.com/main/rss/hotnews/hotnews.rss")
-    @text_url.setAutoresizingMask(NSViewMinXMargin|NSViewMinYMargin|NSViewWidthSizable)
+    @text_url.stringValue = "http://images.apple.com/main/rss/hotnews/hotnews.rss"
+    @text_url.autoresizingMask = NSViewMinXMargin|NSViewMinYMargin|NSViewWidthSizable
     app.window.contentView.addSubview(@text_url)
 
     button = NSButton.alloc.initWithFrame(NSMakeRect(415, 324, 61, 32))
-    button.setTitle("Get")
-    button.setAction("retrieveRSSFeed:")
-    button.setTarget(self)
-    button.setBezelStyle(NSRoundedBezelStyle)
-    button.setAutoresizingMask(NSViewMinXMargin|NSViewMinYMargin)
+    button.title = "Get"
+    button.action = "retrieveRSSFeed:"
+    button.target = self
+    button.bezelStyle = NSRoundedBezelStyle
+    button.autoresizingMask = NSViewMinXMargin|NSViewMinYMargin
     app.window.contentView.addSubview(button)
 
     scroll_view = NSScrollView.alloc.initWithFrame(NSMakeRect(0, 0, 480, 322))
-    scroll_view.setAutoresizingMask(NSViewMinXMargin|NSViewMinYMargin|NSViewWidthSizable|NSViewHeightSizable)
-    scroll_view.setHasVerticalScroller(true)
+    scroll_view.autoresizingMask = NSViewMinXMargin|NSViewMinYMargin|NSViewWidthSizable|NSViewHeightSizable
+    scroll_view.hasVerticalScroller = true
     app.window.contentView.addSubview(scroll_view)
 
     @table_view = NSTableView.alloc.init
     column_title = NSTableColumn.alloc.initWithIdentifier("title")
     column_title.editable = false
     column_title.headerCell.setTitle("Title")
-    column_title.setWidth(400)
+    column_title.width = 400
     @table_view.addTableColumn(column_title)
 
     column_date = NSTableColumn.alloc.initWithIdentifier("date")
     column_date.editable = false
     column_date.headerCell.setTitle("Date")
-    column_date.setWidth(400)
+    column_date.width = 400
     @table_view.addTableColumn(column_date)
 
     @table_view.delegate = self
     @table_view.dataSource = self
-    @table_view.setAutoresizingMask(NSViewMinXMargin|NSViewMaxXMargin|NSViewMinYMargin|NSViewMaxYMargin)
-    @table_view.setDoubleAction("doubleClickColumn:")
-    @table_view.setTarget(self)
+    @table_view.autoresizingMask = NSViewMinXMargin|NSViewMaxXMargin|NSViewMinYMargin|NSViewMaxYMargin
+    @table_view.target = self
+    @table_view.doubleAction = :"doubleClickColumn:"
 
     scroll_view.setDocumentView(@table_view)
   end
@@ -91,5 +90,4 @@ class RssReaderController
       end
     end
   end
-
 end
