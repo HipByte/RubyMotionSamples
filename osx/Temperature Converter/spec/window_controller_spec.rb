c51c238e94a4ef16706d9857e5e2f59d323d28ea
Watson <watson1978@gmail.com>
2014-01-31 09:57:59 +0900

    fix a spec in Temperature Converter
    Looks like NSFormCell does not have insertText property

--- osx/Temperature Converter/spec/window_controller_spec.rb
+++ osx/Temperature Converter/spec/window_controller_spec.rb
@@ -9,7 +9,7 @@ describe 'WindowController' do
   
   it '32°C for Celsius' do
     view = @controller.form.cellAtIndex(1)
-    view.insertText = "32"
+    view.title = "32"
     view = @controller.form.cellAtIndex(2)
     # fahrenheit = view.stringValue
     # fahrenheit.should.equal '89,6'
