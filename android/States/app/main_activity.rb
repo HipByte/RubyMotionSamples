f596ed8269681d32bf33b5552eac13623e96d55f
Mark Rickert <mjar81@gmail.com>
2014-08-14 09:44:57 -0400

    Fix R::Layout issue I thought was a bug but isn't.

--- android/States/app/main_activity.rb
+++ android/States/app/main_activity.rb
@@ -10,15 +10,7 @@ class MainActivity < Android::App::Activity
   end
 
   def adapter
-    # 17367043 is Android::R.layout.simple_list_item_1
-    # Calling Android::R.layout.simple_list_item_1 with RM 3.0 b0.3 results in:
-    #
-    # Exception raised: NoMethodError: undefined method `layout' for android.R:java.lang.Class
-    #
-    # Documentation of R.layout:
-    # http://developer.android.com/reference/android/R.layout.html
-
-    Android::Widget::ArrayAdapter.new(self, 17367043, States.all)
+    Android::Widget::ArrayAdapter.new(self, Android::R::Layout::Simple_list_item_1, States.all)
   end
 
 end
