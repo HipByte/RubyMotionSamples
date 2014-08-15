dd4ec93a157141bdce7c18e945ca9027a00f3906
Mark Rickert <mjar81@gmail.com>
2014-08-15 11:48:26 -0400

    Not sure what I'm doing wrong here...

--- android/States/app/main_activity.rb
+++ android/States/app/main_activity.rb
@@ -5,12 +5,17 @@ class MainActivity < Android::App::Activity
 
     list = Android::Widget::ListView.new(self)
     list.adapter = adapter
+    list.onItemSelectedListener = self
 
     self.contentView = list
   end
 
   def adapter
     Android::Widget::ArrayAdapter.new(self, Android::R::Layout::Simple_list_item_1, States.all)
+  end
+
+  def onItemSelected(parent, view, position, id)
+    puts 'clicked'
   end
 
 end
